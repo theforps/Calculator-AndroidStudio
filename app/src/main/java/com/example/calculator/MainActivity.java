e1b34bf0a575bc67c71ee3a56681c3b91165a737
theforps <theforps@gmail.com>
2022-11-20 16:12:52 +0300

    calc

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        if (view.toString().equals("1"))
+        {
+            System.out.print(true);
+        }
+    }
+}
