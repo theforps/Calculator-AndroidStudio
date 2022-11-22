860dbd4f7263e4d110c6d511b30e92c42d5c708e
theforps <theforps@gmail.com>
2022-11-22 13:57:13 +0300

    Update 21/11/22

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -29,69 +29,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Click(View view) {
-
         Button button = (Button)view;
-        String textOfBut = button.getText().toString();
+        Create(button);
+    }
 
+    public void Create(Button button)
+    {
+        String textOfBut = button.getText().toString();
         TextView label = findViewById(R.id.label);
         String textOfLabel = label.getText().toString();
-
         List<Character> signs = Lists.newArrayList('+','-','×','%','÷','.');
-        DecimalFormat format = new DecimalFormat();
         int maxLenght = 16;
 
-        if (textOfLabel.charAt(0) == '0' && Character.isDigit(textOfBut.charAt(0))
-                && textOfLabel.length() == 1 || textOfLabel.charAt(0) == 'N')
         {
+            if (textOfLabel.charAt(0) == '0' && Character.isDigit(textOfBut.charAt(0))
+                    && textOfLabel.length() == 1 || textOfLabel.charAt(0) == 'N') {
+                textOfLabel = "";
 
-            textOfLabel = "";
-        }
-
-        if (textOfBut.charAt(0) == 'C')
-            textOfLabel = "0";
-        else if (textOfBut.charAt(0) == 'A' && textOfLabel.length() >= 2)
-            textOfLabel = textOfLabel.substring(0,textOfLabel.length()-1);
-        else if (textOfBut.charAt(0) == '=')
-        {
-            boolean check = false;
-            for (int i = 0; i < textOfLabel.length(); i++)
-            {
-                if (signs.contains(textOfLabel.charAt(i)) && i + 1 < textOfLabel.length()
-                        && Character.isDigit(textOfLabel.charAt(i+1)) || textOfLabel.charAt(i) == '%')
-                    check = true;
-            }
-
-            if(check)
-            {
-                Expression e = new Expression(textOfLabel);
-                double resualt = e.calculate();
-
-                if ((int)resualt == resualt)
-                    textOfLabel = Integer.toString((int)resualt);
-                else
-                    textOfLabel = Double.toString(resualt);
-            }
-            else
+            } else if (textOfBut.charAt(0) == 'C')
                 textOfLabel = "0";
-        }
-        else if (Character.isDigit(textOfBut.charAt(0)))
-            textOfLabel += textOfBut;
-        else
-        {
-            if (signs.contains(textOfBut.charAt(0)))
+
+            else if (textOfBut.charAt(0) == 'A' && textOfLabel.length() >= 2)
+                textOfLabel = textOfLabel.substring(0, textOfLabel.length() - 1);
+
+            else if (textOfBut.charAt(0) == '=') {
+                boolean check = false;
+                for (int i = 0; i < textOfLabel.length(); i++) {
+                    if (signs.contains(textOfLabel.charAt(i)) && i + 1 < textOfLabel.length()
+                            && Character.isDigit(textOfLabel.charAt(i + 1)) || textOfLabel.charAt(i) == '%')
+                        check = true;
+                }
+
+                if (check) {
+                    Expression e = new Expression(textOfLabel);
+                    double resualt = e.calculate();
+
+                    if ((int) resualt == resualt)
+                        textOfLabel = Integer.toString((int) resualt);
+                    else
+                        textOfLabel = Double.toString(resualt);
+                } else
+                    textOfLabel = "0";
+            } else if (Character.isDigit(textOfBut.charAt(0)))
                 textOfLabel += textOfBut;
+
+            else {
+                char temp = textOfLabel.charAt(textOfLabel.length() - 1);
+                if (signs.contains(textOfBut.charAt(0)) && !signs.contains(temp))
+                    textOfLabel += textOfBut;
+            }
+
+            if (textOfLabel.length() > maxLenght)
+                textOfLabel = textOfLabel.substring(0, maxLenght);
+
+            if (textOfLabel.contains(","))
+                textOfLabel = textOfLabel.replace(",", "");
         }
-
-
-        if (textOfLabel.length() > maxLenght)
-            textOfLabel = textOfLabel.substring(0,maxLenght);
-
-        if (textOfLabel.contains(","))
-            textOfLabel = textOfLabel.replace(",", "");
-
 
         label.setText(textOfLabel);
-
     }
 
 }
